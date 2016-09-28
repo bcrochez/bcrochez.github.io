6dce4719fc02ac4a82291c4585756a5f39a44457
bcrochez <bastien.crochez@gmail.com>
2016-09-28 09:30:47 +0200

    Little adjustement

--- src/components/ExperienceList.tsx
+++ src/components/ExperienceList.tsx
@@ -12,8 +12,8 @@ export class ExperienceList extends React.Component<ExperienceListProps, Experie
       <div className="experience">
         <h3>My experience</h3>
         <ul>
-          <li>2016 - Internship in the Laboratoire d'Informatique Gaspard Monge (LIGM) - Probabilist real-time system scheduling <br/> <i>Implementation of scheduling algorithms into a real-time simulator in Pyhton</i></li>
-          <li>2015 - Internship in CDIP - Mobile device applications developpement <br/> <i>Implementation of mini-games in JavaScript and native Android applications</i></li>
+          <li>2016 - Internship at the Laboratoire d'Informatique Gaspard Monge (LIGM) - Probabilist real-time system scheduling <br/> <i>Implementation of scheduling algorithms into a real-time simulator in Pyhton</i></li>
+          <li>2015 - Internship at CDIP - Mobile device applications developpement <br/> <i>Implementation of mini-games in JavaScript and native Android applications</i></li>
         </ul>
       </div>
     );
